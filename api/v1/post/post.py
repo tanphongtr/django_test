6ea9ee306b0bf7197f57fb0ecd09b57e7ca0fe5c
Tấn-Phong Trần <phongtran.pix@gmail.com>
2020-09-09 11:32:22 +0700

    Post Filter

--- api/v1/post/post.py
+++ api/v1/post/post.py
@@ -1,15 +1,23 @@
 from rest_framework import status, generics
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
-
 from django_test.models import Post
 from .serializers import PostSerializer
 
-class PostViewSet(generics.ListCreateAPIView):
-    queryset = Post.objects
+from rest_framework import filters
+from rest_framework.pagination import PageNumberPagination, CursorPagination, LimitOffsetPagination
+from django_filters.rest_framework import DjangoFilterBackend
+# from .pagination import LinkHeaderPagination, CustomPagination as CustomPagination2
 
-    print(queryset.query)
+class PostViewSet(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
+    search_fields = ['title', 'content', ]  # field like
+    ordering_fields = '__all__'  # order by
+    ordering = '-created_at'
+    filterset_fields = ['title', ]  # field =
 
     @swagger_auto_schema(
         tags=['Post'],
@@ -17,10 +25,12 @@ class PostViewSet(generics.ListCreateAPIView):
         operation_id='List Post',
         operation_summary='Test',
     )
+    # def get(self, request, *args, **kwargs):
+    #     queryset = self.filter_queryset(self.get_queryset().all())
+    #     serializer = self.get_serializer(queryset, many=True)
+    #     return Response(serializer.data)
     def get(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset().all())
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+        return super().get(request, *args, **kwargs)
 
     @swagger_auto_schema(
         tags=['Post'],
@@ -30,6 +40,7 @@ class PostViewSet(generics.ListCreateAPIView):
     )
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
+
 
 class PostDetailViewSet(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'uuid'
